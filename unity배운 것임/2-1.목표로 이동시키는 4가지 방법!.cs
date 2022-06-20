58ef31ed752105929b8de79932e931818ce569d9
이정민 <102846582+Leejungmin1119@users.noreply.github.com>
2022-06-20 17:02:09 +0900

    Update 2-1.목표로 이동시키는 4가지 방법!.cs

--- unity배운 것임/2-1.목표로 이동시키는 4가지 방법!.cs
+++ unity배운 것임/2-1.목표로 이동시키는 4가지 방법!.cs
@@ -16,19 +16,19 @@ public class 흐름 : MonoBehaviour
 
         //1.MoveTowards(현재 위치,목표위치,속도)
 
-        //transform.position = Vector3.MoveTowards(transform.position, target,1f);
+        transform.position = Vector3.MoveTowards(transform.position, target,1f);
 
         //2.SmoothDamp(부드러운 이동)(현재위치,목표위치,참조속도,속도)
         // !! 마지막 매개면수에 반비례하여 속도가 증가하는 방식임
-        /*
+        
         Vector3 vec = Vector3.zero;
 
         transform.position = Vector3.SmoothDamp(transform.position, target, ref vec, 0.3f);
-        */
+        
 
         //3.Lerp(선형보간)(현재 위치,목표위치,속도(최댓값 1))
 
-        //transform.position = Vector3.Lerp(transform.position, target, 0.03f);
+        transform.position = Vector3.Lerp(transform.position, target, 0.03f);
 
         //4.Slerp (구형 선형 보간)
         transform.position = Vector3.Slerp(transform.position, target, 0.05f);
