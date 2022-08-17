f000aa9952eaa5520515d090e44de12576ecad2f
이정민 <102846582+Leejungmin1119@users.noreply.github.com>
2022-08-17 20:18:52 +0900

    Add files via upload

---
+++ unity배운 것임/6.인터페이스 설정과 기능들.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 인터페이스_설저 : MonoBehaviour
+{
+    //인터페이스 설정!.
+
+    /*
+     1. 인터페이스 설정법.
+
+    오른쪽 우클릭 -> Ui -> Canvas만들기  
+
+    2. Text.
+
+
+    ui -> regacy -> Text클릭
+
+
+
+    font에 글씨체를 적용할 수 있으며 유니티에 먼저 파일을 옮긴뒤 사용가능하다.
+    
+    Alignment : 글의 위치를 바꿀수 있다.(왼쪽,중간,위)
+
+    Horizontal Overflow : 기본설정으로 Warp이 되어있을텐데 Warp은 Text칸에 벗어나면 글을 적지 못하지만 
+                          overflow로 설정하면 칸을 벗어나도 가능하다.
+
+    line spacing : 라인별 띄우는 칸수를 뜻한다.
+
+    3.이미지
+
+    ui -> image클릭
+
+
+    !! texture Type(이미지를 넣을 대상에 있는 설정) : sprite(2d and Ui)로 설정해야 image파일에 넣을 수 있다.
+
+    perserve Aspect : 내가 넣은 이미지에 맞게 최적화시켜준다. =>화질흐림 개선
+
+    image type :( 
+                    tiled :   image크기에 맞게 사진을 복사시켜준다.(안될 수도 있다.) 
+                    filed : 이미지가 어떻게 채워지는지 또는 지워지는지 설정시켜준다.
+                )
+    */
+
+
+}
